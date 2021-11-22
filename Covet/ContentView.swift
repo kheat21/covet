0a14f04e04b691b2562b7893e8abaaa787c91153
Brendan Manning <brendan@trailmapper.io>
2021-11-22 11:15:20 -0500

    Initial Commit

---
+++ Covet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covet
+//
+//  Created by Brendan Manning on 11/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covet/CovetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovetApp.swift
+//  Covet
+//
+//  Created by Brendan Manning on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct CovetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
