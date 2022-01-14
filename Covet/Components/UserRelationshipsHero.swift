e27d2a90209d3aac54b54621f48ece6a59d28f5c
Brendan Manning <BrendanAndrewManning@gmail.com>
2022-01-14 11:05:46 -0500

    Create UserRelationshipsHero.swift

---
+++ Covet/Components/UserRelationshipsHero.swift
@@ -0,0 +1,57 @@
+//
+//  UserRelationshipsHero.swift
+//  Covet
+//
+//  Created by Covet on 1/13/22.
+//
+
+import SwiftUI
+
+struct UserRelationshipsHero: View {
+    
+    var following: [CovetUser]
+    var followers: [CovetUser]
+    var friends: [CovetUser]
+    
+    var body: some View {
+        HStack {
+            NavigationLink(
+                destination: UserManagerView(relationshipTypes: [UserRelationshipSearchType.FRIENDS])
+            ) {
+                VStack {
+                    Text(String(followers.count))
+                        .font(.system(size: 24, weight: .bold, design: .rounded))
+                    Text("followers")
+                }
+            }
+            NavigationLink(
+                destination: UserManagerView(relationshipTypes: [UserRelationshipSearchType.FOLLOWINGS])
+            ) {
+                VStack {
+                    Text(String(following.count))
+                        .font(.system(size: 24, weight: .bold, design: .rounded))
+                    Text("following")
+                }
+            }
+            NavigationLink(
+                destination: UserManagerView(relationshipTypes: [UserRelationshipSearchType.FRIENDS])) {
+                VStack {
+                    Text(String(friends.count))
+                        .font(.system(size: 24, weight: .bold, design: .rounded))
+                    Text("friends")
+                }
+            }
+        }
+        .frame(width: nil, height: 60, alignment: .top)
+    }
+}
+
+struct UserRelationshipsHero_Previews: PreviewProvider {
+    static var previews: some View {
+        UserRelationshipsHero(
+            following: [],
+            followers: [],
+            friends: []
+        )
+    }
+}
