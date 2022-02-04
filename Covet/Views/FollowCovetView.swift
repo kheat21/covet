d9f1cd2ef7d55d814f96be7a5f7f38b1c2d74f7f
Brendan Manning <BrendanAndrewManning@gmail.com>
2022-02-03 21:18:27 -0500

    Make this its own component

---
+++ Covet/Views/FollowCovetView.swift
@@ -0,0 +1,54 @@
+//
+//  FollowCovetView.swift
+//  Covet
+//
+//  Created by Covet on 2/3/22.
+//
+
+import AlertToast
+import SwiftUI
+
+struct FollowCovetView: View {
+    @EnvironmentObject var auth: AuthService
+    @State var isProcessingCovetFollow: Bool = false
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Looks like you're new!")
+                .font(.system(.headline))
+                .padding([.bottom], 8)
+                .padding([.leading, .trailing], 32)
+            Text("Why don't you start seeing some products by following @Covet?")
+                .font(.system(.subheadline))
+                .padding([.bottom], 8)
+                .padding([.leading, .trailing], 32)
+                .multilineTextAlignment(.center)
+            Button(action: {
+                if !self.isProcessingCovetFollow {
+                    followCovet()
+                }
+            }, label: {
+                Text("Follow @Covet")
+                    .padding(.all, 8)
+                    .foregroundColor(Color.white)
+            })
+                .background(Color.covetGreen())
+                .cornerRadius(4)
+            Spacer()
+        }
+        .toast(isPresenting: $isProcessingCovetFollow, alert: {
+            AlertToast(displayMode: .alert, type: .loading, title: nil)
+        })
+    }
+    private func followCovet() {
+        Task {
+            self.isProcessingCovetFollow = true
+            do {
+                if let relationship = try await API.followCovet() {
+                    await auth.refreshUser()
+                }
+            } catch {}
+            self.isProcessingCovetFollow = false
+        }
+    }
+}
